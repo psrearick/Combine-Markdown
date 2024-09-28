a965e8164ccc62a02e7f95e0442db0f8c8fba9d3
Phillip Rearick <psrearick@gmail.com>
2024-09-28 19:44:28 -0400

    wip

--- m5.py
+++ m5.py
@@ -1,27 +1,55 @@
 from run_combine_markdown import run_combine_markdown
+import os
 import argparse
 
-def main():
-    parser = argparse.ArgumentParser(description="Combine Markdown files for The Modular Five.")
-    parser.add_argument("-c", "--category", help="Root Folder")
-    parser.add_argument("-i", "--item", help="Item")
-    args = parser.parse_args()
-
-    category = args.category
-    item = args.item
-    root = "/Users/philliprearick/Home-Local/Active/Personal/Writing/The Modular Five/Version 2/Source"
-    source_path = f"{root}/Comprehensive Reference/{category}"
+def process(category, item, source_type, process_all=False, recursive=False):
+    root = "/Users/philliprearick/Home-Local/Active/Personal/Writing/The Modular Five/Version 2"
     output_path = f"{root}/Combination"
+    source_path = f"{root}/Source/{source_type}"
 
-    source_path = f"{source_path}/{item}" if item else source_path
+    if category:
+        output_path = f"{output_path}/{source_type}"
+        source_path = f"{source_path}/{category}"
+        if item:
+            output_path = f"{output_path}/{category}"
+            source_path = f"{source_path}/{item}"
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    if process_all and not item:
+        all_items = os.listdir(source_path)
+        for sub_item in all_items:
+            item_path = os.path.join(source_path, sub_item)
+            if os.path.isdir(item_path):
+                if category:
+                    process(category, sub_item, source_type, False, recursive)
+                    continue
+                process(sub_item, None, source_type, recursive, recursive)
+
+        if not recursive:
+            return
 
     run_combine_markdown(
-        "/Users/philliprearick/Home-Local/Active/Personal/Writing/The Modular Five/Version 2/Source/Comprehensive Reference/Items",
+        source_path,
         # yaml_file="/path/to/your/order.yaml",
-        output_file="/Users/philliprearick/Home-Local/Active/Personal/Writing/The Modular Five/Version 2/Combination",
+        output_file=output_path,
         include_all=True,
         keep_numbers=False
     )
+
+def main():
+    parser = argparse.ArgumentParser(description="Combine Markdown files for The Modular Five.")
+    parser.add_argument("-t", "--type", help="Type")
+    parser.add_argument("-c", "--category", help="Category")
+    parser.add_argument("-i", "--item", help="Item")
+    parser.add_argument("-a", "--all", action="store_true", help="Include all markdown files")
+    parser.add_argument("-r", "--recursive", action="store_true", help="Include all markdown files, recursively")
+    args = parser.parse_args()
+
+    source_type = args.type if args.type else "Comprehensive Reference"
+
+    process(args.category, args.item, source_type, args.all, args.recursive)
 
 if __name__ == '__main__':
     main()
